13502c16f19f7a80cd9d1736839daf1c570c8984
SasakaRoben <sasakarobenld@gmail.com>
2026-03-23 20:12:58 +0300

    Eliminate the `last_digit` variable entirely.
    Since we use it immediately each time

--- projects/ch4-expressions/reverse.c
+++ projects/ch4-expressions/reverse.c
@@ -5,15 +5,12 @@ int main() {
     printf("Enter an 3-digit integer: ");
     scanf("%d", &num);
 
-    int last_digit;
-    last_digit = num % 10;
-    reversed = last_digit * 100;
+    reversed = (num % 10) * 100;
     num /= 10;
-    last_digit = num % 10;
-    reversed += last_digit * 10;
+    reversed += (num % 10) * 10;
     num /= 10;
-    last_digit = num % 10;
-    reversed += last_digit;
+    reversed += num % 10;
+    
     printf("Reversed number: %d\n", reversed);
     return 0;
 }
